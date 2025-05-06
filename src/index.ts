b95703fc90c29cdde069c90ba2904277319dd78b
Scover <bardini.raphael@gmail.com>
2025-05-06 23:30:02 +0200

    1.2.0

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,11 @@ import { writeFileSync } from 'fs';
 import { processFile } from './process-file';
 
 function main(): void {
-	const files = process.argv.slice(2);
-
-	files.forEach(file => {
-		const fileOutput = processFile(file);
-
-		console.info(`Writing ${file}.ts`);
-
-		writeFileSync(`${file}.ts`, fileOutput);
-	});
+	for (const file of process.argv.slice(2)) {
+		const filename = `${file}.generated.ts`;
+		console.info('Writing', filename);
+		writeFileSync(filename, processFile(file));
+	}
 }
 
 main();
