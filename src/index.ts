424309a1736226a594b8e8fd9ec1a2434e95eb30
Raphaël BARDINI <dj6664@engie.com>
2025-05-12 15:39:18 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,10 @@ function main(): void {
 	const args = process.argv.slice(2);
 	for (let i = 0; i < args.length; ++i) {
 		const file = args[i]!;
-		const outFile = args[i + 1] ?? `${file}.generated.ts`;
+		const outFile = args[i + 1];
+		if (outFile === undefined) i++;
 		console.info('Writing', outFile);
-		writeFileSync(outFile, processFile(file));
+		writeFileSync(outFile ?? `${file}.generated.ts`, processFile(file));
 	}
 }
 
